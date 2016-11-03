c90dc4385c43f390fff4fe9393e777b3f8eec1bc
Stephen Sabsowitz <sabsowitz@yahoo.com>
2016-11-03 17:37:03 -0400

    Building out rover navigation

--- assets/js/components/app.routes.js
+++ assets/js/components/app.routes.js
@@ -12,51 +12,57 @@ CosmosApp.config(function($stateProvider, $urlRouterProvider) {
 
         // route for the explore page
         .state('explore', {
-            url: '/mothership&explore',
+            url: '/explore',
             templateUrl : 'pages/explore.html',
             controller  : 'ExploreController'
         })
         // sine wave state
         .state('explore.sine', {
-            url: '/mothership&explore&sine',
+            url: '&sine',
             templateUrl : 'pages/explore.sine.html',
             controller : 'ExploreController'
         })
         // apod state
         .state('explore.apod', {
-            url: '/mothership&explore&apod',
+            url: '&apod',
             templateUrl : 'pages/explore.apod.html',
             controller : 'ExploreController'
         })
         // mars rover state
-        .state('explore.rover', {
-            url: '/mothership&explore&rover',
+        .state('explore.mars', {
+            url: '&rover',
             templateUrl : 'pages/explore.mars.html',
+            controller : 'MarsController'
+        })
+        // mars rover "Curiosity" state
+        .state('explore.mars.curiosity', {
+            url: '&curiosity',
+            templateUrl : 'pages/rover.curiosity.html',
             controller : 'MarsController'
         })
         // particle state
         .state('explore.particles', {
-            url: '/mothership&explore&particles',
+            url: '&particles',
             templateUrl : 'pages/explore.particles.html',
             controller : 'ExploreController'
         })
         // circle state
         .state('explore.circles', {
-            url: '/mothership&explore&circles',
+            url: '&circles',
             templateUrl: 'pages/explore.circles.html',
             controller: 'ExploreController'
         })
 
         // route for the art page
         .state('art', {
-            url: '/mothership&art',
+            url: '/art',
             templateUrl : 'pages/art.html',
             controller  : 'artController'
         })
 
         // route for the contact page
         .state('contact', {
-            url: '/mothership&contact',
+            url: '/contact',
             templateUrl : 'pages/contact.html'
         })
 });
