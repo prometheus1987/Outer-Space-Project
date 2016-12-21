adc530b1ec7887179aae5f8de606a4213f5ec933
Stephen Sabsowitz <sabsowitz@yahoo.com>
2016-12-21 18:08:26 -0500

    updated .gitignore

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var sourcemaps = require("gulp-sourcemaps");
+var babel = require("gulp-babel");
+var concat = require("gulp-concat");
+var plugins = require('gulp-load-plugins')();
+var pipes = {};
+
+var paths = {
+    scripts: ['./app/assets/js/components/*.js', './app/assets/js/explore/*.js'],
+    styles: ['./app/assets/css/*.css', './app/assets/*.scss'],
+    index: './app/index.html',
+    partials: ['./app/views/*.html', '!index.html'],
+    dist: './public'
+};
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('./app/assets/js/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('./app/assets/css/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('public/css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('./app/assets/js/**/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('public'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/js'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('./app/assets/js/**/*.js', ['lint', 'scripts']);
+    gulp.watch('./app/assets/css/*.scss', ['sass']);
+});
+
+// Babel Task
+gulp.task("babel", function () {
+    return gulp.src("./app/assets/js/**/*.js")
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(concat("all.js"))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("public"));
+});
+
+// Default Task
+gulp.task('default', ['lint', 'sass', 'scripts', 'watch', 'babel']);
---
+++ karma.conf.js
@@ -0,0 +1,28 @@
+module.exports = function(config) {
+  config.set({
+    basePath: 'C:/Users/HackerOne/src/Digital-Cosmos',
+    frameworks: ['jasmine'],
+    files: [
+      'node_modules/jquery/public/jquery.js',
+      'node_modules/angular/angular.js',
+      'node_modules/angular-ui-router/release/angular-ui-router.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'app/assets/js/components/app.module.js',
+      'app/assets/js/components/app.controllers.js',
+      'app/assets/js/components/app.routes.js',
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome', 'Firefox'],
+    singleRun: false,
+    concurrency: Infinity
+  })
+};
+    
