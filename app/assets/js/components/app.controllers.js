caad87ac929677234461606f509dc7d8a7d10be7
sabsowitz <sabsowitz@yahoo.com>
2016-12-27 09:40:20 -0500

    Refactoring APOD

--- app/assets/js/components/app.controllers.js
+++ app/assets/js/components/app.controllers.js
@@ -7,32 +7,19 @@ CosmosApp.controller('ExploreController', function($scope, $http) {
   });
 
   // api call for nasa apod
-  let url = "https://api.nasa.gov/planetary/apod?";
-  let key = "api_key=NeHYhGtJMXT1kJ9jSP8bnRF2t1IpYShALfGkSKoz";
+  $scope.url = "https://api.nasa.gov/planetary/apod?";
+  $scope.key = "api_key=NeHYhGtJMXT1kJ9jSP8bnRF2t1IpYShALfGkSKoz";
 
-  $.ajax({
-      url: url + key,
-      success: handleResult
-  });
-
-  function handleResult(result) {
-      if ("copyright" in result) {
-          $("#copyright").text("Image Credits: " + result.copyright);
-      } else {
-          $("#copyright").text("Image Credits: " + "Public Domain");
-      }
-      if (result.media_type == "video") {
-          $("#apod_img_id").css("display", "none");
-          $("#apod_vid_id").attr("src", result.url);
-      } else {
-          $("#apod_vid_id").css("display", "none");
-          $("#apod_img_id").attr("src", result.url);
-      }
-      $("#reqObject").text(url);
-      $("#returnObject").text(JSON.stringify(result, null, 4));
-      $("#explaination").text(result.explanation);
-      $("#title").text(result.title);
-  }
+  $http.get($scope.url + $scope.key)
+      .success(function(data) {
+          $scope.apod = data;
+          $scope.title = $scope.apod.title;
+          $scope.hdurl = $scope.apod.hdurl;
+          $scope.explanation = $scope.apod.explanation;
+      })
+      .error(function(error){
+          console.log(error);
+      });
 });
 
 CosmosApp.controller('RoverController', function($scope, $http) {
@@ -61,7 +48,7 @@ CosmosApp.controller('RoverController', function($scope, $http) {
     // request for curiosity
     $http.get($scope.baseUrl + $scope.curiosity_rover +  $scope.date_params + $scope.key)
         .success(function(result) {
-            $scope.photos = result.photos;
+            $scope.curiosity_photos = result.photos;
         })
         .error(function(error){
             console.log(error);
