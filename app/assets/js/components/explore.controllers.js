49cea93716bb291dd9ecccf82204798a3518f586
sabsowitz <sabsowitz@yahoo.com>
2017-01-19 12:45:19 -0500

    refactoring Explore Controller

--- app/assets/js/components/explore.controllers.js
+++ app/assets/js/components/explore.controllers.js
@@ -3,19 +3,13 @@
 
     CosmosApp.controller('ExploreController', function($scope, $http) {
 
-        $scope.url = "https://api.nasa.gov/planetary/apod?";
+        // set variables
+        $scope.baseUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/";
+        $scope.curiosityRover = ["Curiosity"];
+        $scope.opportunityRover = ["Opportunity"];
+        $scope.dateParams = "/photos?earth_date=";
+        $scope.apodURL = "https://api.nasa.gov/planetary/apod?";
         $scope.key = "api_key=NeHYhGtJMXT1kJ9jSP8bnRF2t1IpYShALfGkSKoz";
-
-        // api call for nasa apod
-        $http.get($scope.url + $scope.key)
-            .success(function(data) {
-                $scope.title = data.title;
-                $scope.hdurl = data.hdurl;
-                $scope.explanation = data.explanation;
-            })
-            .error(function(error){
-                console.log(error);
-            });
 
         // calculate date for rover requests
         var today = new Date();
@@ -32,15 +26,19 @@
         if(month<10) {
             month='0'+month;
         }
-        today = year+ '-' +month+ '-' +day;
-        opportunityLatest = year+ '-' +month+ '-' +delayedDay;
+        today = year+ '-' +month+ '-' +day+ '&';
+        opportunityLatest = year+ '-' +month+ '-' +delayedDay+ '&';
 
-        // set variables
-        $scope.baseUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/";
-        $scope.curiosityRover = ["Curiosity"];
-        $scope.opportunityRover = ["Opportunity"];
-        $scope.dateParams = "/photos?earth_date=";
-        $scope.key = "&api_key=NeHYhGtJMXT1kJ9jSP8bnRF2t1IpYShALfGkSKoz";
+        // api call for nasa apod
+        $http.get($scope.apodURL + $scope.key)
+            .success(function(data) {
+                $scope.title = data.title;
+                $scope.hdurl = data.hdurl;
+                $scope.explanation = data.explanation;
+            })
+            .error(function(error){
+                console.log(error);
+            });
 
         // request for curiosity
         $http.get($scope.baseUrl + $scope.curiosityRover +  $scope.dateParams + today + $scope.key)
@@ -50,6 +48,7 @@
             .error(function(error){
 
             });
+
 
         // request for opportunity
         $http.get($scope.baseUrl + $scope.opportunityRover + $scope.dateParams + opportunityLatest + $scope.key)
