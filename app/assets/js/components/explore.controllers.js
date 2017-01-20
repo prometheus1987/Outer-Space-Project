341642d7ed5068152b3aca81aedff4bd016b7e00
Stephen Sabsowitz <sabsowitz@yahoo.com>
2017-01-20 18:46:09 -0500

    Refactoring Explore Controller

--- app/assets/js/components/explore.controllers.js
+++ app/assets/js/components/explore.controllers.js
@@ -5,20 +5,18 @@
 
         // set variables
         $scope.baseUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/";
+        $scope.apodURL = "https://api.nasa.gov/planetary/apod?";
         $scope.curiosityRover = ["Curiosity"];
         $scope.opportunityRover = ["Opportunity"];
         $scope.dateParams = "/photos?earth_date=";
-        $scope.apodURL = "https://api.nasa.gov/planetary/apod?";
         $scope.key = "api_key=NeHYhGtJMXT1kJ9jSP8bnRF2t1IpYShALfGkSKoz";
 
         // calculate date for rover requests
-        var today = new Date();
-        var opportunityLatest = new Date();
+        let today = new Date();
 
-        var day = today.getDate()-1;
-        var delayedDay = today.getDate()-6;
-        var month = today.getMonth()+1;
-        var year = today.getFullYear();
+        let day = today.getDate()-1;
+        let month = today.getMonth()+1;
+        let year = today.getFullYear();
 
         if(day<10) {
             day='0'+day;
@@ -27,7 +25,6 @@
             month='0'+month;
         }
         today = year+ '-' +month+ '-' +day+ '&';
-        opportunityLatest = year+ '-' +month+ '-' +delayedDay+ '&';
 
         // api call for nasa apod
         $scope.retrieveApodData = function() {
@@ -46,7 +43,8 @@
         $scope.retrieveCuriosityData = function() {
             $http.get($scope.baseUrl + $scope.curiosityRover +  $scope.dateParams + today + $scope.key)
                 .success(function(result) {
-                    $scope.curiosity_photos = result.photos;
+                    $scope.photos = result.photos;
+                    console.log($scope.photos.length);
                 })
                 .error(function(error){
                     console.log(error);
@@ -55,9 +53,13 @@
 
         // request for opportunity
         $scope.retrieveOpportunityData = function() {
-            $http.get($scope.baseUrl + $scope.opportunityRover + $scope.dateParams + opportunityLatest + $scope.key)
+
+            $scope.delayedDateFlag = true;
+
+            $http.get($scope.baseUrl + $scope.opportunityRover + $scope.dateParams + today + $scope.key)
                 .success(function(result) {
-                    $scope.opportunity_photos = result.photos;
+                    $scope.photos = result.photos;
+                    console.log($scope.photos.length);
                 })
                 .error(function(error){
                     console.log(error);
