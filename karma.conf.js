5ad43fdf6a9d83d6f6e3a328e2007847edb3f915
Stephen Sabsowitz <sabsowitz@yahoo.com>
2017-01-24 21:39:25 -0500

    Karma correctly configured. Wrote first test for Explore Controller.

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,18 @@
 module.exports = function(config) {
   config.set({
-    basePath: 'C:/Users/HackerOne/src/Digital-Cosmos',
+    basePath: __dirname,
     frameworks: ['jasmine'],
     files: [
-      '/node_modules/jquery/public/jquery.js',
+      'node_modules/jquery/dist/jquery.js',
       'node_modules/angular/angular.js',
       'node_modules/angular-ui-router/release/angular-ui-router.js',
       'node_modules/angular-mocks/angular-mocks.js',
+      'node_modules/angular-ui-bootstrap/dist/ui-bootstrap.js',
+      'node_modules/angular-animate/angular-animate.js',
       'app/assets/js/components/app.module.js',
       'app/assets/js/components/explore.controllers.js',
       'app/assets/js/components/app.routes.js',
-      'spec/explore.spec.js'
+      'app/assets/js/components/explore.spec.js'
     ],
     exclude: [
     ],
