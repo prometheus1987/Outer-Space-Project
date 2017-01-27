0619253682cbf145ad6af474636b01c4a7df7022
Stephen Sabsowitz <sabsowitz@yahoo.com>
2017-01-26 19:39:38 -0500

    - removed vm.asteroidUrl - Building out APOD unit test spec - reformatted img src for Curiosity rover

--- app/assets/js/components/explore.controllers.js
+++ app/assets/js/components/explore.controllers.js
@@ -15,7 +15,6 @@
 
         vm.baseUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/";
         vm.apodUrl = "https://api.nasa.gov/planetary/apod?";
-        vm.asteroidUrl = "https://api.nasa.gov/neo/rest/v1/neo/browse?";
         vm.rover = [];
         vm.dateParams = "/photos?earth_date=";
         vm.key = "api_key=NeHYhGtJMXT1kJ9jSP8bnRF2t1IpYShALfGkSKoz";
--- app/assets/js/components/explore.spec.js
+++ app/assets/js/components/explore.spec.js
@@ -14,3 +14,47 @@ describe('ExploreController', function() {
     expect(ExploreController).toBeDefined();
   });
 });
+
+describe('APOD Request', function() {
+
+  var $httpBackend;
+  var url = "https://api.nasa.gov/planetary/apod?";
+
+  var responseSuccess =
+  {
+    "date": "Today's Date",
+    "explanation": "A super awesome explanation of the picture.",
+    "hdurl": "An awesome image",
+    "title": "Dark Side of the Moon",
+  };
+
+  beforeEach(module('app'));
+
+  beforeEach(inject(function(_$q_, _$httpBackend_) {
+    $q = _$q_;
+    $httpBackend = _$httpBackend_;
+  }));
+
+  it('should demonstrate using when (200 status)', inject(function($http) {
+
+    $http.get(url)
+      .success(function(data) {
+        $scope.valid = true;
+        $scope.response = data;
+      })
+      .error(function(data, status, headers, config) {
+        $scope.valid = false;
+      });
+
+    $httpBackend
+      .when('GET', 'http://localhost/foo')
+      .respond(200, { foo: 'bar' });
+
+    $httpBackend.flush();
+
+    expect($scope.valid).toBe(true);
+    expect($scope.response).toEqual({ foo: 'bar' });
+
+  }));
+});
+
