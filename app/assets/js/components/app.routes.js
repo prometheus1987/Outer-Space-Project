aca8230b66992386d5efe9eb24a16435afd6f028
sabsowitz <sabsowitz@yahoo.com>
2017-02-22 17:28:14 -0500

    minor refactors

--- app/assets/js/components/app.routes.js
+++ app/assets/js/components/app.routes.js
@@ -8,41 +8,40 @@
     $urlRouterProvider.otherwise('/');
 
     $stateProvider
-    // route for the home page
       .state('home', {
           url: '/',
           templateUrl: 'app/views/home.html'
       })
-      // route for the explore page
+
       .state('explore', {
           url: '/explore',
           templateUrl: 'app/views/explore.html',
           controller: 'RoverController'
       })
-      // apod state
+
       .state('explore.apod', {
           url: '&apod',
           templateUrl: 'app/views/explore.apod.html',
           controller: 'ApodController',
           controllerAs: 'apod'
       })
-      // mars rover state
+
       .state('explore.rover', {
           url: '/rover/:rover',
-          templateUrl: 'app/views/mars.rover.html',
+          templateUrl: 'app/views/explore.rover.html',
           controller: 'RoverController',
           controllerAs: 'rover',
           params: {
             rover: 'curiosity'
           }
       })
-      // explore sounds state
+      
       .state('explore.sounds', {
           url: '&sounds',
           templateUrl: 'app/views/explore.sounds.html',
           controller: 'RoverController'
       })
-      // route for the contact page
+
       .state('contact', {
           url: '/contact',
           templateUrl : 'app/views/contact.html'
