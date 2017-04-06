982d32f5346c754fe1cc7e28bed20d03e7bd18db
Stephen Sabsowitz <sabsowitz@yahoo.com>
2017-04-06 11:16:59 -0400

    building out orbital data request into view

--- app/assets/js/components/orbital.controller.js
+++ app/assets/js/components/orbital.controller.js
@@ -5,7 +5,7 @@
     .module('app')
     .controller('OrbitalController',
 
-      function($http) {
+      function retrieveData($http) {
 
         const key = "api_key=NeHYhGtJMXT1kJ9jSP8bnRF2t1IpYShALfGkSKoz";
         const url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-03-09&end_date=2017-03-16&";
@@ -17,13 +17,25 @@
         function retrieveOrbitalData() {
           $http.get(url + key)
             .success(function (data) {
+              vm.data = mapOrbitals(data["near_earth_objects"]["2017-03-09"]);
               vm.count = data["element_count"];
-              console.log(vm.count);
               console.log(data["near_earth_objects"]["2017-03-09"]);
             })
             .error(function (error) {
               console.log(error);
             });
         }
-      })
+      });
+
+      function mapOrbitals(data) {
+          return _.map(data, function(orbital) {
+              return {
+                  name: orbital.name,
+                  magnitude: orbital.absolute_magnitude_h,
+                  diameterMin: orbital.estimated_diameter.kilometers.estimated_diameter_min,
+                  diameterMax: orbital.estimated_diameter.kilometers.estimated_diameter_max
+
+              }
+          });
+      }
 })();
