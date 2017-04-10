7ec983faf595e476b298f3d73d3a9fa1f75898aa
Stephen Sabsowitz <sabsowitz@yahoo.com>
2017-04-10 13:03:46 -0400

    orbital refactoring

--- app/assets/js/components/orbital.controller.js
+++ app/assets/js/components/orbital.controller.js
@@ -5,21 +5,42 @@
         .module('app')
         .controller('OrbitalController',
 
-            function retrieveData($http) {
+            function($http) {
 
                 const key = "api_key=NeHYhGtJMXT1kJ9jSP8bnRF2t1IpYShALfGkSKoz";
-                const url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-03-09&end_date=2017-03-16&";
+                const url = "https://api.nasa.gov/neo/rest/v1/";
 
                 let vm = this;
 
                 vm.retrieveOrbitalData = retrieveOrbitalData;
 
+                function getDate() {
+
+                    let date = new Date();
+                    let day = date.getDate();
+                    let month = date.getMonth() + 1;
+                    let year = date.getFullYear();
+
+                    if (day < 10) {
+                        day = '0' + day;
+                    }
+                    if (month < 10) {
+                        month = '0' + month;
+                    }
+
+                    date = year + '-' + month + '-' + day;
+                    return date;
+                }
+
                 function retrieveOrbitalData() {
-                    $http.get(url + key)
+
+                    let date = getDate();
+                    let query = "feed?start_date=";
+
+                    $http.get(url + query + date + "&" + key)
                         .success(function (data) {
-                            vm.data = mapOrbitals(data["near_earth_objects"]["2017-03-09"]);
+                            vm.data = mapOrbitals(data["near_earth_objects"][date]);
                             vm.count = data["element_count"];
-                            console.log(data["near_earth_objects"]["2017-03-09"]);
                         })
                         .error(function (error) {
                             console.log(error);
@@ -36,7 +57,8 @@
                 diameterMax: orbital.estimated_diameter.kilometers.estimated_diameter_max,
                 hazardous: orbital.is_potentially_hazardous_asteroid,
                 orbitingBody: orbital.close_approach_data[0].orbiting_body,
-                approachDate: orbital.close_approach_data[0].close_approach_date
+                approachDate: orbital.close_approach_data[0].close_approach_date,
+                distance: orbital.close_approach_data[0].miss_distance.kilometers
             }
         });
     }
