db2ffdf360adfef6624c1f6489c6f5a506764d8b
Stephen Sabsowitz <sabsowitz@yahoo.com>
2017-05-24 09:28:50 -0400

    files

--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,9 @@
       './node_modules/jquery/dist/jquery.js',
       './node_modules/lodash/lodash.js',
       './node_modules/moment/moment.js',
+      './node_modules/angular-material/angular-material.js',
+      './node_modules/angular-animate/angular-animate.js',
+      './node_modules/angular-aria/angular-aria.js',
       './node_modules/material-design-lite/material.js'
     ]
   };
