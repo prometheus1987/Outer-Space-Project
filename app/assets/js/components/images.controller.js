c85198e2720a2d8b57f6ffb5ac84d086b6a349ef
Stephen Sabsowitz <sabsowitz@yahoo.com>
2017-07-26 10:22:16 -0400

    working image search functionality. Actual searchbox needs to be implemented

--- app/assets/js/components/images.controller.js
+++ app/assets/js/components/images.controller.js
@@ -6,30 +6,30 @@
 
     function imagesController($http) {
 
-            const url = "http://images-api.nasa.gov/search?q=jupiter";
+            const url = "http://images-api.nasa.gov/search?q=";
+            let query = "Mars";
             let vm = this;
 
-
-        $http.get(url)
+            $http.get(url + query)
                 .then(successfulResponse, errorResponse);
 
             function successfulResponse(res) {
                 let results = res.data.collection.items;
                 vm.images = mapImages(results);
-                console.log(results);
             }
 
             function errorResponse(error) {
                 console.log(error);
             }
 
-        function mapImages(results) {
-            return _.map(results, function (image) {
-                return {
-                    title: image.data[0].title,
-                    link: image.links[0].href
-                }
-            });
-        }
+            function mapImages(results) {
+                return _.map(results, function (image) {
+                    return {
+                        title: image.data[0].title,
+                        link: image.links[0].href
+                    }
+                });
+            }
+
     }
 })();
