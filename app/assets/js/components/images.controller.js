f2b48cf39c22cf0dd800c0197a923313868d6384
Stephen Sabsowitz <sabsowitz@yahoo.com>
2017-10-05 09:59:34 -0400

    - change to images controller - fixed YARN issue

--- app/assets/js/components/images.controller.js
+++ app/assets/js/components/images.controller.js
@@ -8,10 +8,13 @@
 
         const url = "https://images-api.nasa.gov/search?q=";
         let vm = this;
+        let noImages = false;
 
         vm.search = function() {
             $http.get(url + vm.query)
                 .then(successfulResponse, errorResponse);
+
+            let noImages = false;
 
             function successfulResponse(res) {
                 let results = res.data.collection.items;
@@ -19,17 +22,21 @@
             }
 
             function errorResponse(error) {
+                vm.noImages = true;
                 console.log(error);
             }
 
             function mapImages(results) {
                 return _.map(results, function (image) {
                     return {
+                        type: image.data[0].media_type,
                         title: image.data[0].title,
+                        description: image.data[0].description,
                         link: image.links[0].href
                     }
                 });
             }
         }
     }
+
 })();
